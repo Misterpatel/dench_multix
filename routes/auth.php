5836a8fbfa2b1fcd50967e95f7d44b65e8338b54
gbyte-dev <gbytedeveloper@gmail.com>
2025-04-04 15:50:03 +0530

    changes

--- routes/auth.php
+++ routes/auth.php
@@ -38,7 +38,7 @@ use Illuminate\Support\Facades\Route;
 // });
  
 Route::middleware('auth')->group(function () {
-	
+    
     Route::get('verify-email', EmailVerificationPromptController::class)
                 ->name('verification.notice');
 
